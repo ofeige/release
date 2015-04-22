9e790728caca1c8194f783af586403593be8c36d
Oliver Feige <oliver@feige.net>
2015-04-22 08:02:02 +0200

    remove apc cache

--- web/app.php
+++ web/app.php
@@ -5,10 +5,6 @@ use Symfony\Component\HttpFoundation\Request;
 
 $loader = require_once __DIR__.'/../app/bootstrap.php.cache';
 
-$apcLoader = new ApcClassLoader('release.dev', $loader);
-$loader->unregister();
-$apcLoader->register(true);
-
 require_once __DIR__.'/../app/AppKernel.php';
 
 $kernel = new AppKernel('prod', false);
